c0d6a47b7f95e4288f7867c335a24993e8a40781
desrinaH <64583175+desrinaH@users.noreply.github.com>
2022-03-26 09:02:22 +0700

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const Post = require('../model/Posts');
+const verify = require('./verifytoken');
+
 
 //GET ALL POSTS
-router.get('/', async (req, res) => {
+router.get('/', verify, async (req, res) => {
     try {
         const posts = await Post.find();
         res.status(200).json(posts);
@@ -13,8 +15,8 @@ router.get('/', async (req, res) => {
 });
 
 //SUBMIT POSTS
-router.post('/', async (req, res) => {
-    if(!req.body.country || !req.body.kind || !req.body.etag || !req.body.items){
+router.post('/', verify, async (req, res) => {
+    if(!req.body){
         res.status(400).send({message: "Content can not be empty or invalid"});
         return;
     }
@@ -22,7 +24,12 @@ router.post('/', async (req, res) => {
         kind: req.body.kind,
         country: req.body.country,
         etag: req.body.etag,
-        items: req.body.items
+        kindItem: req.body.kindItem,
+        etagItem: req.body.etagItem,
+        id: req.body.id,
+        channelId: req.body.channelId,
+        title: req.body.title,
+        assignable: req.body.assignable
     });
     try {
         const savePost = await post.save();
@@ -34,65 +41,107 @@ router.post('/', async (req, res) => {
 });
 
 //GET SPECIFIC POST
-router.get('/:postId/:index', async (req, res) => {
-    try{
-        const post = await Post.findById(req.params.postId);
-        res.status(200).json(post);
-    } catch (error){
-        res.status(404).json({message: "Content not found"});
+router.get('/:postId', verify,  async (req, res) => {
+    const post = await Post.findById(req.params.postId);
+    if(!post){
+        return res.status(404).json({message: "Content not found"});
+    } else {
+        try{
+           res.status(200).json(post);
+        } catch (error){
+          res.status(500).json({message: "Error occured"});
+        }
     }
 });
 
-router.get('/country/:country', async (req,res) => {
-    try{ 
-        const where = await Post.find( {country: req.params.country} );
-        res.status(200).json(where);
-    } catch (error){
-        res.status(404).json({message: "Content not found"});
+router.get('/country/:country', verify, async (req,res) => {
+    const where = await Post.find( {country: req.params.country} );
+    if(!where){
+        return res.status(404).json({message: "Content not found"});
+       
+    } else {
+     try{ 
+           res.status(200).json(where);
+     } catch (error){
+            res.status(500).json({message: "Error occured"});
+        }
     }
 });  
+
+router.get('/id/:id', verify, async (req,res) => {
+    const item = await Post.find( {id: req.params.id} );
+    //const item = await where.find({id: req.params.id});
+    if(!item){
+        return res.status(404).json({message: "Content not found"});
+       
+    } else {
+     try{ 
+           res.status(200).json(item);
+     } catch (error){
+            res.status(500).json({message: "Error occured"});
+        }
+    }
+}); 
 
 //DELETE A POST
-router.delete('/:postId/', async (req, res) => {
-    try{
+router.delete('/:postId/', verify, async (req, res) => {
+   
         const removePost = await Post.remove({_id: req.params.postId});
-       // if(!removePost){
-     //       res.status(404).send({message: "Content not found"});
-   //     } else {
-            res.status(200).json(removePost);
- //       }
-
-    } catch (error){
-        res.status(500).send({message: "Error occured"});
-    }
+        if(!removePost){
+           return res.status(404).send({message: "Content not found"});
+        } else{
+            try {
+                res.status(200).json(removePost);
+            } catch (error) {
+                res.status(500).send({message: "Error occured"});
+            }
+        }
+            
 });
 
-router.delete('/country/:country', async (req,res) => {
-    try{ 
-        const removeCountry = await Post.remove( {country: req.params.country} );
-        if(!removeCountry){
-            res.status(404).send({message: "Content not found"});
-        } else {
-            res.status(200).json(removeCountry);
-        }
+// router.delete('/etagItem/:etagItem', async (req,res) => { 
+//         const removeItem = await Post.findOneAndDelete( {etagItem: req.params.etagItem} );
+//         if(!removeItem){
+//             return res.status(404).send({message: "Content not found"});
+//         } else {
+//             try{ 
+//             res.status(200).json(removeItem);
+//             } catch (error){
+//                 res.status(500).send({message: "Error occured"});
+//             }
+//         }
         
-    } catch (error){
-        res.status(500).send({message: "Error occured"});
-    }
-});  
+
+// });  
 
 //UPDATE DATA
-router.patch('/:postId', async (req, res) => {
+router.patch('/country/:postId', verify, async (req, res) => {
     if(!req.body){
-        return res.status(400).send({message: "Content to update can not be emppty"});
-    }
+        return res.status(400).send({message: "Content to update can not be empty"});
+    } else{
     try{
         const updatePostCountry = await Post.updateOne({_id: req.params.postId},
             {$set: {country: req.body.country}}
             );
         res.status(200).json(updatePostCountry);
     } catch (error){
-        res.json({message: error});
+        res.status(500).json({message: "Error occured"});
+    }
+    }
+});
+
+router.patch('/id/:postId', verify, async (req, res) => {
+    if(!req.body){
+        return res.status(400).send({message: "Content to update can not be empty"});
+    } else{
+    try{
+        const updatePostId = await Post.updateOne({_id: req.params.postId},
+            {$set: {id: req.body.id}}
+            );
+        res.status(200).json(updatePostId);
+    } catch (error){
+        res.status(500).json({message: "Error occured"});
+    }
     }
 });
 
